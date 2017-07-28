336a62d1d210ccb6c06a59b69ea63ca0b21b84e5
Alexey Chizhevsky <AChyzheuski@iba.by>
2017-07-28 11:27:09 +0300

    add connecting to auth0 to include autorisation

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -4,9 +4,22 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { AUTH_CONFIG } from './auth0-variables';
 import { tokenNotExpired } from 'angular2-jwt';
 
+declare var auth0: any;
+
 @Injectable()
 export class AuthService {
+  auth0 = new auth0.WebAuth({
+    clientID: AUTH_CONFIG.CLIENT_ID,
+    domain: AUTH_CONFIG.CLIENT_DOMAIN
+  });
 
-  constructor() { }
+  loggedIn: boolean;
+  loggedIn$ = new BehaviorSubject<boolean>(this.loggedIn);
+
+  constructor(private router: Router) { 
+    if (this.authenticated) {
+      this.setLoggedIn(true);
+    }
+   }
 
 }
